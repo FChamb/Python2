cd0826eaefb8190ea2251e6b58b469743a10527e
James Hendry <jh455@st-andrews.ac.uk>
2024-03-08 11:55:46 +0000

    Make setup print out instructions to activate venv

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Setup the python virtual environment
 # To activate the virtual environment (before running any python files)
-#  > source code/.venv/bin/activate
 python3 -m venv code/.venv && \
-code/.venv/bin/pip install -r code/requirements.txt
-
+code/.venv/bin/pip install -r code/requirements.txt && \
+echo "====================================" && \
+echo "Virtual environment successfully created" && \
+echo "Remember to activate with:" && \
+echo "> source code/.venv/bin/activate"

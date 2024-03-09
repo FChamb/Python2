ecc744fbfb1dcba221a78ba2348eed4051e1d323
James Hendry <jh455@st-andrews.ac.uk>
2024-03-09 17:24:26 +0000

    Use enums for the different options
    This allows you to capture both the description
    and the key in a simple way.

--- code/MicroDataTeachingVarsTest.py
+++ code/MicroDataTeachingVarsTest.py
@@ -8,12 +8,18 @@ class TestExampleMicroData(unittest.TestCase):
 
     # Helper functions
     def check_valid(self, values, permitted):
+        if permitted is None:
+            return True
         s = pd.Series(values)
-        self.assertTrue(col_valid(s, permitted))
+        if col_valid(s, permitted.keys()):
+            return True
+        for v in values:
+            if not col_valid(pd.Series(v), permitted.keys()):
+                self.fail(f"Expected {v} be valid, but it was not (permitted: {permitted.keys()})")
 
     def check_invalid_single(self, value, permitted):
         s = pd.Series([value])
-        self.assertFalse(col_valid(s, permitted))
+        self.assertFalse(col_valid(s, permitted.keys()))
 
     # Student ID
     def test_valid_student_id(self):
@@ -35,142 +41,141 @@ class TestExampleMicroData(unittest.TestCase):
             "E12000009",
             "W92000004"
         ]
-        self.check_valid(valid_regions, md.regions)
+        self.check_valid(valid_regions, md.RegionOptions)
 
     def test_invalid_regions(self):
-        self.check_invalid_single("S12000001", md.regions)
-        self.check_invalid_single(60, md.regions)
+        self.check_invalid_single("S12000001", md.RegionOptions)
+        self.check_invalid_single(60, md.RegionOptions)
 
     # Residence types
     def test_residence_type(self):
-        self.check_valid(["H", "C"], md.residences)
+        self.check_valid(["H", "C"], md.ResidenceOptions)
 
     def test_invalid_residence_type(self):
-        self.check_invalid_single("O", md.residences)
-        self.check_invalid_single(0, md.residences)
+        self.check_invalid_single("O", md.ResidenceOptions)
+        self.check_invalid_single(0, md.ResidenceOptions)
 
     # Family Composition
     def test_family_composition_valid(self):
-        self.check_valid([3,2,1,4,5,6,-9], md.familyComp)
+        self.check_valid([3,2,1,4,5,6,-9], md.FamilyCompositionOptions)
 
     def test_family_composition_invalid(self):
-        self.check_invalid_single(0, md.familyComp)
-        self.check_invalid_single(-1, md.familyComp)
+        self.check_invalid_single(0, md.FamilyCompositionOptions)
+        self.check_invalid_single(-1, md.FamilyCompositionOptions)
 
     # Population base
     def test_population_base_valid(self):
-        s = pd.Series([1, 2, 3])
-        self.assertTrue(col_valid(s, md.popBase))
+        self.check_valid([1, 2, 3], md.PopulationBaseOptions)
 
     def test_population_base_invalid(self):
-        self.check_invalid_single(0, md.popBase)
-        self.check_invalid_single(-1, md.popBase)
-        self.check_invalid_single(-9, md.popBase)
+        self.check_invalid_single(0, md.PopulationBaseOptions)
+        self.check_invalid_single(-1, md.PopulationBaseOptions)
+        self.check_invalid_single(-9, md.PopulationBaseOptions)
 
     # Sex
     def test_sex_valid(self):
-        self.check_valid([1, 2], md.sexes)
+        self.check_valid([1, 2], md.SexOptions)
 
     def test_sex_invalid_3(self):
-        self.check_invalid_single(0, md.sexes)
-        self.check_invalid_single(3, md.sexes)
-        self.check_invalid_single(-9, md.sexes)
+        self.check_invalid_single(0, md.SexOptions)
+        self.check_invalid_single(3, md.SexOptions)
+        self.check_invalid_single(-9, md.SexOptions)
 
     # Age
     def test_age_valid(self):
-        self.check_valid([1, 2, 3, 4, 5, 6, 7, 8], md.ages)
+        self.check_valid([1, 2, 3, 4, 5, 6, 7, 8], md.AgeOptions)
 
     def test_age_invalid(self):
-        self.check_invalid_single(0, md.ages)
-        self.check_invalid_single(9, md.ages)
-        self.check_invalid_single(-9, md.ages)
+        self.check_invalid_single(0, md.AgeOptions)
+        self.check_invalid_single(9, md.AgeOptions)
+        self.check_invalid_single(-9, md.AgeOptions)
 
     # Marital Status
     def test_marital_status_valid(self):
-        self.check_valid([1,2,3,4,5], md.maritalStat)
+        self.check_valid([1,2,3,4,5], md.MaritalStatusOptions)
 
     def test_marital_status_invalid_zero(self):
-        self.check_invalid_single(0, md.maritalStat)
-        self.check_invalid_single(6, md.maritalStat)
-        self.check_invalid_single(-9, md.maritalStat)
+        self.check_invalid_single(0, md.MaritalStatusOptions)
+        self.check_invalid_single(6, md.MaritalStatusOptions)
+        self.check_invalid_single(-9, md.MaritalStatusOptions)
 
     # Student
     def test_student_valid(self):
-        self.check_valid([1, 2], md.studying)
+        self.check_valid([1, 2], md.StudentOptions)
 
     def test_student_invalid_zero(self):
-        self.check_invalid_single(0, md.studying)
-        self.check_invalid_single(3, md.studying)
-        self.check_invalid_single(-9, md.studying)
+        self.check_invalid_single(0, md.StudentOptions)
+        self.check_invalid_single(3, md.StudentOptions)
+        self.check_invalid_single(-9, md.StudentOptions)
 
     # Country of birth
     def test_birth_country_valid(self):
-        self.check_valid([1, 2, -9], md.birthCountry)
+        self.check_valid([1, 2, -9], md.CountryOfBirthOptions)
 
     def test_birth_country_invalid_zero(self):
-        self.check_invalid_single(0, md.birthCountry)
-        self.check_invalid_single(3, md.birthCountry)
+        self.check_invalid_single(0, md.CountryOfBirthOptions)
+        self.check_invalid_single(3, md.CountryOfBirthOptions)
 
     # Health
     def test_health_valid(self):
-        self.check_valid([1, 2, 3, 4, 5, -9], md.healthStat)
+        self.check_valid([1, 2, 3, 4, 5, -9], md.HealthOptions)
 
     def test_health_invalid(self):
-        self.check_invalid_single(0, md.healthStat)
-        self.check_invalid_single(6, md.healthStat)
+        self.check_invalid_single(0, md.HealthOptions)
+        self.check_invalid_single(6, md.HealthOptions)
 
     # Ethnic Group
     def test_ethnicity_valid(self):
-        self.check_valid([1,2,3,4,5,-9], md.ethnicity)
+        self.check_valid([1,2,3,4,5,-9], md.EthnicityOptions)
 
     def test_ethnicity_invalid(self):
-        self.check_invalid_single(0, md.ethnicity)
-        self.check_invalid_single(6, md.ethnicity)
+        self.check_invalid_single(0, md.EthnicityOptions)
+        self.check_invalid_single(6, md.EthnicityOptions)
 
     # Religion
     def test_religion_valid(self):
-        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.religion)
+        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.ReligionOptions)
 
     def test_religion_invalid(self):
-        self.check_invalid_single(0, md.religion)
-        self.check_invalid_single(10, md.religion)
+        self.check_invalid_single(0, md.ReligionOptions)
+        self.check_invalid_single(10, md.ReligionOptions)
 
     # Economic activity
     def test_economic_activity_valid(self):
-        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.econAct)
+        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.EconomicActivityOptions)
 
     def test_economic_activity_invalid(self):
-        self.check_invalid_single(0, md.econAct)
-        self.check_invalid_single(10, md.econAct)
+        self.check_invalid_single(0, md.EconomicActivityOptions)
+        self.check_invalid_single(10, md.EconomicActivityOptions)
 
     # Occupation
     def test_occupation_valid(self):
-        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.job)
+        self.check_valid([1,2,3,4,5,6,7,8,9,-9], md.OccupationOptions)
 
     def test_occupation_invalid(self):
-        self.check_invalid_single(0, md.job)
-        self.check_invalid_single(10, md.job)
+        self.check_invalid_single(0, md.OccupationOptions)
+        self.check_invalid_single(10, md.OccupationOptions)
 
     # Industry
     def test_industry_valid(self):
-        self.check_valid([1,2,3,4,5,6,7,8,9,10,11,12,-9], md.industry)
+        self.check_valid([1,2,3,4,5,6,7,8,9,10,11,12,-9], md.IndustryOptions)
 
     def test_industry_invalid(self):
-        self.check_invalid_single(0, md.industry)
-        self.check_invalid_single(13, md.industry)
+        self.check_invalid_single(0, md.IndustryOptions)
+        self.check_invalid_single(13, md.IndustryOptions)
 
     # Hours worked per week
     def test_hours_valid(self):
-        self.check_valid([1,2,3,4,-9], md.hrs)
+        self.check_valid([1,2,3,4,-9], md.HoursWorkedPerWeekOptions)
 
     def test_hours_invalid(self):
-        self.check_invalid_single(0, md.hrs)
-        self.check_invalid_single(5, md.hrs)
+        self.check_invalid_single(0, md.HoursWorkedPerWeekOptions)
+        self.check_invalid_single(5, md.HoursWorkedPerWeekOptions)
 
     # Approximated social grade
     def test_social_grade_valid(self):
-        self.check_valid([1,2,3,4,-9], md.socialGr)
+        self.check_valid([1,2,3,4,-9], md.SocialGradeOptions)
 
     def test_social_grade_invalid(self):
-        self.check_invalid_single(0, md.socialGr)
-        self.check_invalid_single(5, md.socialGr)
+        self.check_invalid_single(0, md.SocialGradeOptions)
+        self.check_invalid_single(5, md.SocialGradeOptions)

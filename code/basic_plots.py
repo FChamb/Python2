83b954f9d62703ca33ecc084510bbe3f026d77ad
sphterry <sphterry@gmail.com>
2024-03-13 15:13:39 +0000

    Implemented basic bar plot

--- code/basic_plots.py
+++ code/basic_plots.py
@@ -1,9 +1,9 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from MicroDataTeachingVars import *
 
-csvPath = 'data/census2011-clean.csv' #placeholder
+csvPath = '../data/census2011-clean.csv' #placeholder
 
 ''' TO DO:
         - implement plot functions
@@ -12,11 +12,31 @@ csvPath = 'data/census2011-clean.csv' #placeholder
 
 def main():
     df = pd.read_csv(csvPath)
-    genRecordBarPlot(df, 'Region')
+    genRecordBarPlot(df, 't')
     genRecordBarPlot(df, 'Occupation')
-    genDistPieChart(df, 'Age')
-    genDistPieChart(df, 'Economic Activity')
+    #genDistPieChart(df, 'Age')
+    #genDistPieChart(df, 'Economic Activity')
 
 def genRecordBarPlot(df, colName):
+    if colMap.get(colName) is not None:
+        values = [str(x) for x in colMap.get(colName).values] # gets options as strings
+        # refer to for legend stuff
+        # https://stackoverflow.com/questions/62941033/how-to-turn-x-axis-values-into-a-legend-for-matplotlib-bar-graph 
+        f, ax = plt.subplots()
+        ax.bar(values, df[colName].value_counts()) # plot options w/ their frequencies
+        
+        plt.title("Number of records for each " + colName.lower())
+        plt.xlabel(colName)
+        plt.xticks(values)
+        plt.ylabel("Number of Records")
+        # create legend w/ keys and descriptions
+        legend = '\n'.join(f'{x.key()} - {x.desc()}' for x in colMap.get(colName).options)
+        # TO DO: ADD LEGEND SOMEHOW
+        plt.show()
+    else:
+        raise ValueError(colName+" is an invalid column")
 
-def genDistPieChart(df, colName):
+#def genDistPieChart(df, colName):
+
+if __name__ == "__main__":
+    main()

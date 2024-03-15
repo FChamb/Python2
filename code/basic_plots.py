9f90b6431a43b5c031139b446a4515101d256f80
sphterry <sphterry@gmail.com>
2024-03-15 15:32:33 +0000

    Added comments to basic_plots.py

--- code/basic_plots.py
+++ code/basic_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from MicroDataTeachingVars import *
+from MicroDataTeachingVars import colMap
 
 csvPath = '../data/census2011-clean.csv' #placeholder
 imagesDir = '../images/'
@@ -14,39 +14,48 @@ imagesDir = '../images/'
 
 def main():
     df = pd.read_csv(csvPath)
+    print("Generating basic requirement plots...")
     genRecordBarPlot(df, 'Region')
     genRecordBarPlot(df, 'Occupation')
     genDistPieChart(df, 'Age')
     genDistPieChart(df, 'Economic Activity')
+    print("Done.")
 
 def genRecordBarPlot(df, colName):
     if colMap.get(colName) is not None:
-        values = [str(x) for x in colMap.get(colName).values] # gets options as strings
-        # refer to for legend stuff
-        # https://stackoverflow.com/questions/62941033/how-to-turn-x-axis-values-into-a-legend-for-matplotlib-bar-graph 
+        # gets options as strings
+        values = [str(x) for x in colMap.get(colName).values]
+        # plot options w/ their frequencies
         f, ax = plt.subplots()
-        ax.bar(values, df[colName].value_counts()) # plot options w/ their frequencies
-        
+        ax.bar(values, df[colName].value_counts()) 
+        # title + y axis
         plt.title("Number of records for each " + colName.lower())
+        plt.ylabel("Number of Records")
+        # x axis
         plt.xlabel(colName)
         plt.xticks(values)
-        if len(values[0]) > 1:
+        if len(values[0]) > 1: # if vals too long, tilt
             plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-        plt.ylabel("Number of Records")
+
         # create legend w/ keys and descriptions
+        # refer to for legend stuff
+        # https://stackoverflow.com/questions/62941033/how-to-turn-x-axis-values-into-a-legend-for-matplotlib-bar-graph 
         legend = '\n'.join(f'{x.key()} - {x.desc()}' for x in colMap.get(colName).options)
         # TO DO: ADD LEGEND SOMEHOW
+        
+        # save and close
         plt.savefig(imagesDir+'barchart-'+colName.replace(' ', '-').lower()+'.png', bbox_inches="tight")
-        plt.show()
+        plt.close()
     else:
         raise ValueError(colName+" is an invalid column")
 
 def genDistPieChart(df, colName):
     if colMap.get(colName) is not None:
-        plt.pie(df[colName].value_counts(), labels = colMap.get(colName).options)
-        plt.title("Distribution of sample by " + colName.lower())
-        plt.savefig(imagesDir+'piechart-'+colName.replace(' ', '-').lower()+'.png', bbox_inches="tight")
-        plt.show()
+        plt.pie(df[colName].value_counts(), labels = colMap.get(colName).options) # plot w labels
+        plt.title("Distribution of sample by " + colName.lower()) # name
+        # save and close
+        plt.savefig(imagesDir+'piechart-'+colName.replace(' ', '-').lower()+'.png', bbox_inches="tight") 
+        plt.close()
     else:
         raise ValueError(colName+" is an invalid column")
 

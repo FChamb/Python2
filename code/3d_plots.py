1572935153549c4b32515b21df24c4504d5dbbd7
sphterry <sphterry@gmail.com>
2024-03-15 15:04:53 +0000

    Change params and add labels to 3d plot

--- code/3d_plots.py
+++ code/3d_plots.py
@@ -4,17 +4,26 @@ import pandas as pd
 
 from mpl_toolkits import mplot3d
 from statistics import getGroupTable
+from MicroDataTeachingVars import colMap
 
 def main():
     df = pd.read_csv("../data/census2011-clean.csv")
-    plotTable(getGroupTable(df, "Occupation", "Approximated Social Grade"))
+    #plotTable(df, "Region", "Industry")
+    plotTable(df, "Occupation", "Approximated Social Grade")
 
-def plotTable(df):
+def plotTable(df, col1, col2):
+    data = getGroupTable(df, col1, col2)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    for x, z in df.items():
+    for x, z in data.items():
         print("Plotting...", x[0], x[1], z)
         ax.scatter3D(x[0], x[1], z)
+    plt.title("Records by "+col1+" and "+ col2)
+    ax.set_xlabel(col1)
+    ax.set_xticks(colMap.get(col1).values)
+    ax.set_ylabel(col2)
+    ax.set_yticks(colMap.get(col2).values)
+    ax.set_zlabel("Count")
     plt.show()
 
 if __name__ == "__main__":

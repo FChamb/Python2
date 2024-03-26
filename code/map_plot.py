2ae09093a7c5f7ddcd854a7649a2062ce2b7a656
sphterry <sphterry@gmail.com>
2024-03-26 13:36:02 +0000

    Map by region working

--- code/map_plot.py
+++ code/map_plot.py
@@ -16,6 +16,7 @@ from folium.plugins import FastMarkerCluster, HeatMap
 https://focaalvarez.medium.com/mapping-the-uk-and-navigating-the-post-code-maze-4898e758b82f
 https://medium.com/@patohara60/interactive-mapping-in-python-with-uk-census-data-6e571c60ff4
 https://stackoverflow.com/questions/46775667/plotting-uk-districts-postcode-areas-and-regions
+https://realpython.com/python-folium-web-maps-from-data/
 '''
 
 #https://sdgdata.gov.uk/sdg-data/geojson-output-regions.html
@@ -23,15 +24,16 @@ uk = 'https://sdgdata.gov.uk/sdg-data/en/geojson/regions/indicator_8-10-1.geojso
 
 def main():
     df = pd.read_csv("../data/census2011-clean.csv")
-    plotMap(df)
+    plotMap(df, "Ethnic Group")
 
-def plotMap(df):
+def plotMap(df, col): # eventually change to take any column
+    # add check for column validity
     m = folium.Map(location=[55,4], zoom_start=5)
-    folium.GeoJson(uk).add_to(m)
-    """ folium.Choropleth( geo_data = state_geo,
+    folium.Choropleth( geo_data = uk,
                         data = df,
-                        columns=["Region", "Population Base"]) """
-    #folium.LayerControl().add_to(map)
+                        columns=["Region", col],
+                        key_on="feature.properties.geocode",
+                        fill_color = "RdYlGn_r").add_to(m)
     m.show_in_browser()
 
 if __name__ == "__main__":

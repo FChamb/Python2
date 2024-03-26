90eb6aede1289cd46483a0b77b4fd4a6e6b8fa56
sphterry <sphterry@gmail.com>
2024-03-26 20:06:42 +0000

    Better plotMap using findthatpostcode api, still has busted legends

--- code/map_plot.py
+++ code/map_plot.py
@@ -1,10 +1,4 @@
-""" import numpy as np 
-import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
- """
-#import json
-#import geopy
+
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import pandas as pd
@@ -13,12 +7,14 @@ from branca.utilities import split_six
 from folium.plugins import FastMarkerCluster, HeatMap
 
 from stats import getGroupTable
+from census_microdata_2011 import dataset
 
 '''
 https://focaalvarez.medium.com/mapping-the-uk-and-navigating-the-post-code-maze-4898e758b82f
 https://medium.com/@patohara60/interactive-mapping-in-python-with-uk-census-data-6e571c60ff4
 https://stackoverflow.com/questions/46775667/plotting-uk-districts-postcode-areas-and-regions
 https://realpython.com/python-folium-web-maps-from-data/ THIS IS MAIN SOURCE
+https://stackoverflow.com/questions/54595931/show-different-pop-ups-for-different-polygons-in-a-geojson-folium-python-ma 
 '''
 
 #https://sdgdata.gov.uk/sdg-data/geojson-output-regions.html
@@ -26,37 +22,35 @@ eng = 'https://sdgdata.gov.uk/sdg-data/en/geojson/regions/indicator_8-10-1.geojs
 #https://findthatpostcode.uk/areas/W92000004.html
 wales = 'https://findthatpostcode.uk/areas/W92000004.geojson'
 
+ftp_url = 'https://findthatpostcode.uk/areas/'
+
 def main():
     df = pd.read_csv("../data/census2011-clean.csv")
     getTableHtml(df, "E12000001", "Marital Status")
     plotMap(df, "Marital Status")
 
-def plotMap(df, col): # eventually change to take any column
-    # add check for column validity
+def plotMap(df, col):
     m = folium.Map(location=[54.38, -2.7], zoom_start=5)
-    # remove legend from wales
-    #https://stackoverflow.com/questions/54595931/show-different-pop-ups-for-different-polygons-in-a-geojson-folium-python-ma 
-    w = folium.Choropleth(geo_data = wales,
-                            data = df,
-                            columns = ["Region",col],
-                            fill_color = "RdYlGn_r",
-                            fill_opacity=.8,
-                            key_on="feature.properties.code").add_to(m)
-    folium.GeoJsonTooltip(["code"], aliases=[getTableHtml(df,"W92000004",col)]).add_to(w.geojson)
-    folium.Choropleth( geo_data = eng,
-                        data = df,
-                        columns=["Region", col],
-                        key_on="feature.properties.geocode",
-                        fill_color = "RdYlGn_r",
-                        fill_opacity=.8,
-                        legend_name=col+" per Region").add_to(m)
+    for reg in dataset.get_column("Region").values:
+        data = ftp_url + reg + ".geojson"
+        f = folium.Choropleth(geo_data = data,
+                                data = df,
+                                columns = ["Region",col],
+                                fill_color = "RdYlGn_r",
+                                fill_opacity=.8,
+                                key_on="feature.properties.code")
+        folium.GeoJsonTooltip(["code"], aliases=[getTableHtml(df,reg,col)]).add_to(f.geojson)
+        f.add_to(m)
+    """ for key in m._children:
+        if key.startswith('color_map'):
+            del(m._children[key]) """
     m.show_in_browser()
 
 def getTableHtml(df, region, col):
     table = getGroupTable(df, "Region", col) # get unique counts of col
     t = table[table["Region"].values == region] # filter to just specified region
     t =t.drop(labels="Region",axis=1) # remove region column
-    return t.to_html(index=False)
+    return t.to_html(index=False) # return as html w/ no indices
 
 if __name__ == "__main__":
     main()

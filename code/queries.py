1963044294c3eba612721cd49f7ab5851001bcf6
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-03-26 16:23:22 +0000

    Added part 3 of queries req

--- code/queries.py
+++ code/queries.py
@@ -1,8 +1,28 @@
+import sys
+import os
 import pandas as pd
+from os import R_OK
 
 
-def main():
-    df = pd.read_csv("data/census2011-clean.csv")
+def main(csvPath):
+    assert os.path.isfile(csvPath) and os.access(csvPath, R_OK), \
+        f"File {csvPath} does not exist or is not readable"
+
+    df = pd.read_csv(csvPath)
+    findQueries(df)
+    discrepancies = findDiscrepancies(df)
+    print("Discrepancies found between student status and economic activity: ")
+    print(discrepancies)
+
+    hours = findWorkingHours(df)
+    print("Working hours found per week for students: ")
+    print(hours)
+
+def findWorkingHours(df):
+    hours = df[(df["Economic Activity"].isin([4, 6])) & df["Student"] == 1]["Hours worked per week"].sum()
+    return hours
+
+def findQueries(df):
     query1 = find_economically_active_region(df)
     query2 = find_economically_active_region(df)
     print("Number of economically active people by region: ")
@@ -10,6 +30,9 @@ def main():
     print("Number of economically active people by age: ")
     print(query2)
 
+def findDiscrepancies(df):
+    ret = df[df["Student"] == 1]["Economic Activity"].value_counts()
+    return ret
 
 def find_economically_active_region(df):
     df = df[df["Economic Activity"] != -9]
@@ -22,5 +45,8 @@ def find_economically_active_age(df):
 
 
 if __name__ == '__main__':
-    # TODO: Take file as argument
-    main()
+    if len(sys.argv) != 2:
+        print("Invalid arguments")
+        print("Usage:", sys.argv[0], "<csvFile>")
+        exit(1)
+    main(sys.argv[1])
